048f4d490aa3512ce6f6c75608a2793023f69911
GGlin <lgz98616@gmail.com>
2021-04-17 03:06:01 +0800

    Done

--- query.py
+++ query.py
@@ -1,6 +1,10 @@
 import os
 import pickle
+import h5py
+import numpy as np
+from numpy import linalg as LNG
 from string import punctuation
+from collections import Counter
 
 from nltk import word_tokenize, PorterStemmer
 
@@ -13,26 +17,56 @@ FILE_TERM_DOC_PAIRS = 'output.txt'
 SAVE = False  # Save inverted_index else load
 
 
-def output(ll):
-    if ll is None or len(ll) == 0:
-        print('No matched document was found')
-        return
+def save_w():
+    N = len(docId_to_doc)
+    all_terms = [x[2] for x in inverted_index]
+    tfs = np.zeros(shape=(len(all_terms), len(docId_to_doc)), dtype=np.int)
+    dfs = np.zeros(shape=(len(all_terms)), dtype=np.int)
+    w = np.zeros(shape=(len(all_terms), len(docId_to_doc)), dtype=np.float)
+    for docId, doc in docId_to_doc.items():
+        doc = os.path.join(INPUT_DIR, doc)
+        with open(doc, 'rt', encoding='utf-8') as f:
+            texts = f.read()
+            terms = preprocess(texts)
+            terms = Counter(terms)
+            for term in terms:
+                index = search(term)
+                if index is None:
+                    print('Error! {} not found!'.format(term))
+                    # exit(-13)
+                    continue
+                tfs[index, docId] = terms[term]
+                dfs[index] += 1
 
-    p = ll.head
-    print('Found {} documents:'.format(len(ll)))
-    while p:
-        print(docId_to_doc[p.data])
-        p = p.next
+    vfunc = np.vectorize(lambda x: 1 if x <= 0 else x)
+    dfs = vfunc(dfs)
+
+    tfs = np.log10(tfs + 1)
+    dfs = np.log10(N / dfs)
+
+    rows = w.shape[0]
+    cols = w.shape[1]
+    for x in range(rows):
+        for y in range(cols):
+            w[x, y] = tfs[x, y] * dfs[x]
+
+    lengths = LNG.norm(w, axis=0)
+
+    h5f = h5py.File('w2.h5', 'w')
+    h5f.create_dataset('w', data=w)
+    h5f.create_dataset("lengths", data=lengths)
+    h5f.close()
 
 
-def calc_size():
-    from sys import getsizeof
-    sz = 0
-    for x in inverted_index:
-        sz += 2  # Frequency in 2 bytes
-        sz += (6 + len(x[1]) * (4 + 3))  # 2 pointers (head & tail), data & next pointer for each element
-        sz += getsizeof(x[2])
-    return sz
+def load_w():
+    h5f = h5py.File('w.h5', 'r')
+    w = h5f.get('w')[()]
+    lengths = h5f.get('lengths')[()]
+    return w, lengths
+
+
+def get_tfidf(t):
+    pass
 
 
 def save_data():
@@ -49,6 +83,26 @@ def load_data():
         for i in range(len(inverted_index)):
             inverted_index[i][1] = LinkedList.from_list(inverted_index[i][1])
     return inverted_index, docId_to_doc, doc_to_docId
+
+
+def search(term):
+    a = 0
+    b = len(inverted_index)
+    i = int((a + b) / 2)
+    while True:
+        ptr = inverted_index[i][2]
+        if ptr == term:
+            return i
+        elif ptr < term:
+            a = i + 1
+        else:
+            b = i - 1
+        if a == b:
+            i = a
+            continue
+        elif a > b:
+            return None
+        i = int((a + b) / 2)
 
 
 def preprocess(terms):
@@ -100,100 +154,36 @@ def search(term):
         i = int((a + b) / 2)
 
 
-def intersect(l1, l2):
-    p1 = l1.head
-    p2 = l2.head
-    ret = LinkedList()
-    while p1 and p2:
-        if p1.data == p2.data:
-            ret.append(p1.data)
-            p1 = p1.next
-            p2 = p2.next
-        elif p1.data < p2.data:
-            p1 = p1.next
-        else:
-            p2 = p2.next
-    return ret
-
-
-def merge(l1, l2):
-    p1 = l1.head
-    p2 = l2.head
-    ret = LinkedList()
-    while p1 and p2:
-        if p1.data == p2.data:
-            ret.append(p1.data)
-            p1 = p1.next
-            p2 = p2.next
-        elif p1.data < p2.data:
-            ret.append(p1.data)
-            p1 = p1.next
-        else:
-            ret.append(p2.data)
-            p2 = p2.next
-    while p1:
-        ret.append(p1.data)
-        p1 = p1.next
-    while p2:
-        ret.append(p2.data)
-        p2 = p2.next
-    return ret
-
-
-def inverse(l):
-    ret = LinkedList()
-    n = len(docId_to_doc) - 1
-    p = l.head
-    i = 0
-    while p and i <= n:
-        if i < p.data:
-            ret.append(i)
-        else:
-            p = p.next
-        i += 1
-    while i <= n:
-        ret.append(i)
-        i += 1
-
-    return ret
-
-
-def query(option, terms):
-    answer = None
+def query(terms, k):
     terms = set(preprocess(terms))
-    indexes = []
+    N = len(docId_to_doc)
+    scores = [[i, 0] for i in range(N)]
     for term in terms:
-        result = search(term)
-        if result is not None:
-            indexes.append(result)
-    indexes.sort(key=lambda i: inverted_index[i][0])
-    if option == 'and':
-        if len(indexes) < 2 or len(indexes) < len(terms):
-            # Only one matched or some terms not found
-            return None
-        l1 = inverted_index[indexes.pop(0)][1]
-        l2 = inverted_index[indexes.pop(0)][1]
-        answer = intersect(l1, l2)
-        while indexes:
-            answer = intersect(answer, inverted_index[indexes.pop(0)][1])
-    elif option == 'or':
-        if len(indexes) < 1:
-            # No matched
-            return None
-        elif len(indexes) == 1:
-            # Skip merging
-            return inverted_index[indexes.pop(0)][1]
-        l1 = inverted_index[indexes.pop(0)][1]
-        l2 = inverted_index[indexes.pop(0)][1]
-        answer = merge(l1, l2)
-        while indexes:
-            answer = merge(answer, inverted_index[indexes.pop(0)][1])
-    elif option == 'not':
-        if len(terms) != 1:
-            return None
-        answer = inverse(inverted_index[indexes.pop(0)][1])
+        termId = search(term)
+        if termId is None:
+            print('{} not found'.format(term))
+            continue
+        ll = inverted_index[termId][1]
+        if ll is None or len(ll) == 0:
+            print('No matched document was found for term {}'.format(term))
+        p = ll.head
+        while p:
+            docId = p.data
+            scores[docId][1] += w[termId, docId]
+            p = p.next
+    for i in range(N):
+        length = lengths[i]
+        if length != 0:
+            scores[i][1] /= length
 
-    return answer
+    scores.sort(key=lambda x: -x[1])
+    scores = scores[:k]
+    print("Top {} documents:".format(k))
+    print('+************+************+')
+    print('*    Doc    *     Score   *')
+    print('+************+************+')
+    for x in scores:
+        print(f"*{'':2}{docId_to_doc[x[0]]:8}{'':2}*{'':3}{x[1]:6.4f}{'':3}*")
 
 
 if SAVE:
@@ -236,15 +226,16 @@ if SAVE:
                 inverted_index[cur_termId][1].append(doc_to_docId[doc])
             prev_doc = doc
     save_data()
+    save_w()
 else:
     inverted_index, docId_to_doc, doc_to_docId = load_data()
-
-# calc_size()
+    w, lengths = load_w()
 
 ### Sample queries ###
-# answer = query('and', 'cat dog')
-answer = query('and', 'libya missile Gaddafi')
-# answer = query('or', 'cat dog')
-# answer = query('not', 'the')
+# query('libya missile Gaddafi', 10)
 
-output(answer)
+cmd = input('Enter your query: ')
+while cmd != '\\q':
+    query(cmd, 10)
+    print()
+    cmd = input('Enter your query: ')
